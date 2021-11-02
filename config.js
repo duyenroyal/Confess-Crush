16688250f41abc19e7d436f8698e7b411bdf70dc
duyenroyal <92727148+duyenroyal@users.noreply.github.com>
2021-11-02 23:16:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     titleWeb: "Xinchao",
-    introTitle: 'Hà Trang thân mến',
+    introTitle: 'Thu Trang thân mến',
     introDesc: `Chào mừng bạn
-    đã đến với bí mật nho nhỏ
+    đã đến với secret
     của Duyen Hoang Le`,
-    btnIntro: 'Okii ạ',
+    btnIntro: 'Oke',
     title: 'Sau này giàu nhớ bao nuôi Duyen nhé 😘',
     desc: 'Hãy trả lời để tiếp tục ',
     btnYes: 'Tất nhiên gòy <33',
     btnNo: 'Không nhé 🙂',
     question: 'Biết mà, ơ hôm nay có gì đặc biệt đúng không nhỉ',
     btnReply: 'Send nè',
-    reply: 'Chinh nhật cụa tui. My birthday <3',
-    mess: 'Bingo. Chúc mừng sinh nhật nhóo',
-    messDesc: 'Chúc bạn mình tuổi mới tươi như hoa và mọi điều tốt đạp nhất sẽ đến với u',
+    reply: 'My birthday, yoo',
+    mess: 'Bingo. Happy Birthday',
+    messDesc: 'Chúc Bone tuổi mới ít khóc lại, cười nhiều lên và mau mau gặp nhau hen',
     btnAccept: 'Click để tuổi mới nhiều tiền',
-    messLink: 'https://www.youtube.com/watch?v=acWSgmDLxRk'  
+    messLink: 'https://www.youtube.com/watch?v=dNCWe_6HAM8'  
 }
